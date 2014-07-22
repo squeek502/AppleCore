6e94de1ce6d43379cc56b057e1d347ebc96d2ef9
squeek <squeek502@hotmail.com>
2014-07-21 18:59:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+AppleCore
+============
+
+An API for modifying the food and hunger mechanics of Minecraft
---
+++ java/squeek/applecore/ModInfo.java
@@ -0,0 +1,7 @@
+package squeek.applecore;
+
+public final class ModInfo
+{
+	public static final String MODID = "AppleCore";
+	public static final String VERSION = "${version}";
+}
