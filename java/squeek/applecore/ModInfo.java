ffbfb93345f77c611536c60814f911c0e55e4afa
squeek <squeek502@hotmail.com>
2014-07-24 21:53:25 -0700

    Improved build.gradle  * Added api and deobf artifact outputs  * Insert api version into package-info.java  * Fixed example mod metadata file not being deleted

--- java/squeek/applecore/ModInfo.java
+++ java/squeek/applecore/ModInfo.java
@@ -5,4 +5,5 @@ public final class ModInfo
 	public static final String MODID = "AppleCore";
 	public static final String MODAPI = "AppleCoreAPI";
 	public static final String VERSION = "${version}";
+	public static final String APIVERSION = "${apiversion}";
 }
--- java/squeek/applecore/api/package-info.java
+++ java/squeek/applecore/api/package-info.java
@@ -1,4 +1,4 @@
-@API(apiVersion = squeek.applecore.ModInfo.VERSION, owner = squeek.applecore.ModInfo.MODID, provides = squeek.applecore.ModInfo.MODAPI)
+@API(apiVersion = "${apiversion}", owner = "AppleCore", provides = "AppleCoreAPI")
 package squeek.applecore.api;
 
 import cpw.mods.fml.common.API;
