924000f7afd182ac285be830f7797d13b8bd0251
squeek <squeek502@hotmail.com>
2014-07-28 19:24:24 -0700

    Only load the example modifications if the dev environment doesn't have any mods that use AppleCore

--- java/squeek/applecore/example/AppleCoreExample.java
+++ java/squeek/applecore/example/AppleCoreExample.java
@@ -4,10 +4,14 @@ import net.minecraftforge.common.MinecraftForge;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import squeek.applecore.ModInfo;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.versioning.ArtifactVersion;
+import cpw.mods.fml.relauncher.Side;
 
 @Mod(modid = ModInfo.MODID + "Example", version = ModInfo.VERSION, dependencies = "")
 public class AppleCoreExample
@@ -20,13 +24,32 @@ public class AppleCoreExample
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
-		MinecraftForge.EVENT_BUS.register(new EatingSpeedModifier());
-		MinecraftForge.EVENT_BUS.register(new ExhaustionModifier());
-		MinecraftForge.EVENT_BUS.register(new FoodEatenResult());
-		MinecraftForge.EVENT_BUS.register(new FoodStatsAdditionCanceler());
-		MinecraftForge.EVENT_BUS.register(new FoodValuesModifier());
-		MinecraftForge.EVENT_BUS.register(new HealthRegenModifier());
-		MinecraftForge.EVENT_BUS.register(new StarvationModifier());
-		MinecraftForge.EVENT_BUS.register(new FoodValuesTooltipHandler());
+		// only add in the test modifications if we are 'alone' in the dev environment
+		boolean otherDependantModsExist = false;
+		for (ModContainer mod : Loader.instance().getActiveModList())
+		{
+			for (ArtifactVersion dependency : mod.getRequirements())
+			{
+				if (dependency.getLabel().equals(ModInfo.MODID))
+				{
+					otherDependantModsExist = true;
+					break;
+				}
+			}
+			if (otherDependantModsExist)
+				break;
+		}
+		if (!otherDependantModsExist)
+		{
+			MinecraftForge.EVENT_BUS.register(new EatingSpeedModifier());
+			MinecraftForge.EVENT_BUS.register(new ExhaustionModifier());
+			MinecraftForge.EVENT_BUS.register(new FoodEatenResult());
+			MinecraftForge.EVENT_BUS.register(new FoodStatsAdditionCanceler());
+			MinecraftForge.EVENT_BUS.register(new FoodValuesModifier());
+			MinecraftForge.EVENT_BUS.register(new HealthRegenModifier());
+			MinecraftForge.EVENT_BUS.register(new StarvationModifier());
+		}
+		if (event.getSide() == Side.CLIENT)
+			MinecraftForge.EVENT_BUS.register(new FoodValuesTooltipHandler());
 	}
 }
