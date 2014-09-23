75482e4c550d86c108b6eece9f409ae09a03b636
squeek <squeek502@hotmail.com>
2014-09-22 20:00:07 -0700

    Fixed unused import

--- java/squeek/applecore/AppleCore.java
+++ java/squeek/applecore/AppleCore.java
@@ -18,7 +18,6 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLInterModComms;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
