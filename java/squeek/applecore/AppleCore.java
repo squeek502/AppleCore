4d9afe95ac005faf36888fbeac40d2572e351535
squeek <squeek502@hotmail.com>
2014-11-08 10:49:09 -0800

    Allow clients without AppleCore to connect to a server with AppleCore  * See #7

--- java/squeek/applecore/AppleCore.java
+++ java/squeek/applecore/AppleCore.java
@@ -26,7 +26,7 @@ import cpw.mods.fml.relauncher.Side;
 @IFMLLoadingPlugin.SortingIndex(-100)
 @IFMLLoadingPlugin.MCVersion("1.7.10")
 @IFMLLoadingPlugin.TransformerExclusions("squeek.applecore.asm")
-@Mod(modid = ModInfo.MODID, version = ModInfo.VERSION)
+@Mod(modid = ModInfo.MODID, version = ModInfo.VERSION, acceptableRemoteVersions="*")
 public class AppleCore implements IFMLLoadingPlugin
 {
 	public static Logger Log = LogManager.getLogger(ModInfo.MODID);
