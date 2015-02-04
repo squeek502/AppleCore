1415c6f20f3471e58438a4d9adb92537a4073ab4
Ryan Liptak <squeek502@hotmail.com>
2015-02-04 15:03:12 -0800

    Tiny API javadoc update

--- java/squeek/applecore/api/AppleCoreAPI.java
+++ java/squeek/applecore/api/AppleCoreAPI.java
@@ -1,10 +1,10 @@
 package squeek.applecore.api;
 
 /**
- * Used to access or mutate various hidden values of the hunger system.
+ * Used to access/mutate various hidden values of the hunger system or fire standard AppleCore events.
  * 
- * See {@link IAppleCoreAccessor} and {@link IAppleCoreMutator} for a list of the available functions.
- * {@link #accessor} and {@link #mutator} will be initialized by AppleCore on startup.
+ * See {@link IAppleCoreAccessor}, {@link IAppleCoreMutator}, and {@link IAppleCoreDispatcher} for a list of the available functions.
+ * {@link #accessor}, {@link #mutator}, and {@link #dispatcher} will be initialized by AppleCore on startup.
  */
 public abstract class AppleCoreAPI
 {
