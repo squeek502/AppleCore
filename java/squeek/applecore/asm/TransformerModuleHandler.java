9ee4d897c930fe0b991e06f1f1503772302e506e
squeek <squeek502@hotmail.com>
2016-04-07 23:33:43 -0700

    Protect against transforming null bytes  - Triggers an OpenModsLib sanity check that makes it seem like AppleCore is causing crashes that it may not be involved in

--- java/squeek/applecore/asm/TransformerModuleHandler.java
+++ java/squeek/applecore/asm/TransformerModuleHandler.java
@@ -30,6 +30,9 @@ public class TransformerModuleHandler implements IClassTransformer
 	@Override
 	public byte[] transform(String name, String transformedName, byte[] basicClass)
 	{
+		if (basicClass == null)
+			return null;
+
 		for (IClassTransformerModule transformerModule : transformerModules)
 		{
 			for (String classToTransform : transformerModule.getClassesToTransform())
